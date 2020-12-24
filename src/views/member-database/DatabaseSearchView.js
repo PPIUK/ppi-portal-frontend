703669b66b1f4ba81d7d0af3b9f0a7c19b62253d
Albertus Andito <aandito7@gmail.com>
2020-12-24 16:49:13 +0000

    in progress: paginate from backend

--- src/views/member-database/DatabaseSearchView.js
+++ src/views/member-database/DatabaseSearchView.js
@@ -11,56 +11,98 @@ const tableStyle = {
 const profileTableCols = [
     {
         title: 'Name',
-        dataIndex: ['fullName'],
+        dataIndex: 'fullName',
         key: 'name',
+        sorter: (a, b) => a.fullName.localeCompare(b.fullName),
     },
     {
         title: 'University',
-        dataIndex: ['university'],
+        dataIndex: 'university',
         key: 'university',
+        sorter: (a, b) => a.university.localeCompare(b.university),
     },
     {
         title: 'Degree Level',
-        dataIndex: ['degreeLevel'],
+        dataIndex: 'degreeLevel',
         key: 'degreeLevel',
+        sorter: (a, b) => a.degreeLevel.localeCompare(b.degreeLevel),
     },
     {
         title: 'Faculty',
-        dataIndex: ['faculty'],
-        key: 'university',
+        dataIndex: 'faculty',
+        key: 'faculty',
+        sorter: (a, b) => a.faculty.localeCompare(b.faculty),
     },
     {
         title: 'Course',
-        dataIndex: ['course'],
+        dataIndex: 'course',
         key: 'course',
+        sorter: (a, b) => a.course.localeCompare(b.course),
     },
     {
         title: 'Branch',
-        dataIndex: ['branch'],
+        dataIndex: 'branch',
         key: 'branch',
+        sorter: (a, b) => a.branch.localeCompare(b.branch),
     },
 ];
 
 function DatabaseSearchView() {
     const [profilesData, setProfilesData] = useState(null);
+    const [paginationData, setPaginationData] = useState({
+        paginate: true,
+        limit: 10,
+        page: 1,
+    });
     const accessToken = localStorage.getItem('oauth-access-token');
-    let config = {
-        headers: {
-            Authorization: 'Bearer '.concat(accessToken),
-        },
+
+    const handleTableChange = (pagination) => {
+        axios
+            .get('/api/profiles/public', {
+                params: pagination,
+                headers: {
+                    Authorization: 'Bearer '.concat(accessToken),
+                },
+            })
+            .then((resp) => {
+                setProfilesData(resp.data.data.profiles);
+                setPaginationData({
+                    paginate: true,
+                    current: resp.data.data.page,
+                    pageSize: resp.data.data.limit,
+                    total: resp.data.data.totalProfiles,
+                });
+            });
     };
 
     useEffect(() => {
         axios
-            .get('/api/profiles/public', config)
-            .then((resp) => setProfilesData(resp.data.data.profiles));
+            .get('/api/profiles/public', {
+                params: paginationData,
+                headers: {
+                    Authorization: 'Bearer '.concat(accessToken),
+                },
+            })
+            .then((resp) => {
+                setProfilesData(resp.data.data.profiles);
+                setPaginationData({
+                    paginate: true,
+                    current: resp.data.data.page,
+                    pageSize: resp.data.data.limit,
+                    total: resp.data.data.totalProfiles,
+                });
+            });
     }, []);
-
     return (
         <Card {...tableStyle}>
             <Typography.Title level={2}>Public Profile Info</Typography.Title>
             {profilesData && (
-                <Table columns={profileTableCols} dataSource={profilesData} />
+                <Table
+                    columns={profileTableCols}
+                    dataSource={profilesData}
+                    pagination={paginationData}
+                    onchange={handleTableChange}
+                />
             )}
             {!profilesData && <Skeleton />}
         </Card>
