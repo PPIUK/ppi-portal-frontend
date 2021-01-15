f0d32c626d2e619c15e3f628e6a37aa059a471b7
unknown <hutama.tefo@gmail.com>
2021-01-16 05:34:23 +0700

    Profile done

--- src/views/profile/OwnProfileView.js
+++ src/views/profile/OwnProfileView.js
@@ -1,37 +1,72 @@
 import React from 'react';
-import { Card, Table, Typography } from 'antd';
-// import { LockOutlined, UserOutlined } from '@ant-design/icons';
-
-const dataSource = [
-    // Call personal profile from here
-];
-
-const columns = [
-    { title: 'Name', dataIndex: 'fullName', key: 'fullName' },
-    { title: 'Date of Birth', dataIndex: 'dob', key: 'dob' },
-    { title: 'Origin City', dataIndex: 'originCity', key: 'originCity'},
-    { title: 'Address', dataIndex: 'addressUK', key: 'address' },
-    { title: 'Postcode UK', dataIndex: 'postcodeUK', key: 'postcodeUK'},
-    { title: 'University', dataIndex: 'university', key:'university'},
-    { title: 'Degree Level', dataIndex: 'degreeLevel', key: 'degreeLevel'},
-    { title: 'Faculty', dataIndex: 'faculty', key: 'faculty'},
-    { title: 'Phone WA', dataIndex:'phoneWA', key: 'phoneWA'},
-    { title: 'Branch', dataIndex: 'branch', key:'branch'},
-    { title: 'Course', dataIndex: 'course', key: 'course'},
-    { title: 'Start Date', dataIndex: 'startDate', key: 'startDate'},
-    { title: 'End Date', dataIndex: 'endDate', key: 'endDate'},
-    { title: 'Funding Source', dataIndex: 'fundingSource', key: 'fundingSource'},
-    { title: 'Email', dataIndex: 'email', key: 'email'},
-    { title: 'Email Personal', dataIndex: 'emailPersonal', key: 'emailPersonal'},
-    { title: 'Facebook', dataIndex: 'facebook', key: 'facebook'},
-    { title: 'Instagram', dataIndex: 'instagram', key: 'instagram'},
-    { title: 'LinkedIn', dataIndex: 'linkedin', key: 'linkedin'},
-];
+import { Card, Typography } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { useAuth } from '../../utils/useAuth';
 
 function OwnProfileView() {
-    return <Typography.Title level={2} style={{ textAlign: 'center' }}>
-        Profile
-    </Typography.Title>;
+    const auth = useAuth();
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(counter + 1);
+        }, 100);
+    }, []);
+
+    useEffect(() => {
+        console.log(counter);
+    }, []);
+
+    return (
+        <Card
+            title={
+                <span>
+                    <UserOutlined /> My Profile
+                </span>
+            }
+        >
+            <Typography.Text>{`Branch           :   ${auth.user.branch}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Full Name        :   ${auth.user.fullName} `}</Typography.Text>
+            <br />
+            <Typography.Text>{`Date of Birth    :   ${new Date(
+                auth.user.dob
+            ).toLocaleDateString()}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Origin City      :   ${auth.user.originCity}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`addressUK        :   ${auth.user.addressUK}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Postcode UK      :   ${auth.user.postcodeUK}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`University       :   ${auth.user.university}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Degree Level     :   ${auth.user.degreeLevel}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Faculty          :   ${auth.user.faculty}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Course           :   ${auth.user.course}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Start Date       :   ${new Date(
+                auth.user.startDate
+            ).toLocaleDateString()}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`End Date         :   ${new Date(
+                auth.user.endDate
+            ).toLocaleDateString()}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Funding Source   :   ${auth.user.fundingSource}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Email            :   ${auth.user.email}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Email Personal   :   ${auth.user.emailPersonal}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`PhoneWA          :   ${auth.user.phoneWA}`}</Typography.Text>
+            <br />
+            <Typography.Text>{`Linkedin         :   ${auth.user.linkedin}`}</Typography.Text>
+            <br />
+        </Card>
+    );
     // Put profile here
     // <Table dataSource={dataSource} columns={columns} />
 }
