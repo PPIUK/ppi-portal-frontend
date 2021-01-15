faac44b5b78d8a0ffa1c8b0612cdb9cf9a3a6926
Raka Gunarto <rakagunarto@gmail.com>
2021-01-16 04:10:31 +0700

    WIP

--- src/views/profile/OwnProfileView.js
+++ src/views/profile/OwnProfileView.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import { Card, Table, Typography } from 'antd';
-// import { LockOutlined, UserOutlined } from '@ant-design/icons';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { Card, Typography } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+import { useAuth } from '../../utils/useAuth';
 
 const dataSource = [
     // Call personal profile from here
@@ -9,29 +12,65 @@ const dataSource = [
 const columns = [
     { title: 'Name', dataIndex: 'fullName', key: 'fullName' },
     { title: 'Date of Birth', dataIndex: 'dob', key: 'dob' },
-    { title: 'Origin City', dataIndex: 'originCity', key: 'originCity'},
+    { title: 'Origin City', dataIndex: 'originCity', key: 'originCity' },
     { title: 'Address', dataIndex: 'addressUK', key: 'address' },
-    { title: 'Postcode UK', dataIndex: 'postcodeUK', key: 'postcodeUK'},
-    { title: 'University', dataIndex: 'university', key:'university'},
-    { title: 'Degree Level', dataIndex: 'degreeLevel', key: 'degreeLevel'},
-    { title: 'Faculty', dataIndex: 'faculty', key: 'faculty'},
-    { title: 'Phone WA', dataIndex:'phoneWA', key: 'phoneWA'},
-    { title: 'Branch', dataIndex: 'branch', key:'branch'},
-    { title: 'Course', dataIndex: 'course', key: 'course'},
-    { title: 'Start Date', dataIndex: 'startDate', key: 'startDate'},
-    { title: 'End Date', dataIndex: 'endDate', key: 'endDate'},
-    { title: 'Funding Source', dataIndex: 'fundingSource', key: 'fundingSource'},
-    { title: 'Email', dataIndex: 'email', key: 'email'},
-    { title: 'Email Personal', dataIndex: 'emailPersonal', key: 'emailPersonal'},
-    { title: 'Facebook', dataIndex: 'facebook', key: 'facebook'},
-    { title: 'Instagram', dataIndex: 'instagram', key: 'instagram'},
-    { title: 'LinkedIn', dataIndex: 'linkedin', key: 'linkedin'},
+    { title: 'Postcode UK', dataIndex: 'postcodeUK', key: 'postcodeUK' },
+    { title: 'University', dataIndex: 'university', key: 'university' },
+    { title: 'Degree Level', dataIndex: 'degreeLevel', key: 'degreeLevel' },
+    { title: 'Faculty', dataIndex: 'faculty', key: 'faculty' },
+    { title: 'Phone WA', dataIndex: 'phoneWA', key: 'phoneWA' },
+    { title: 'Branch', dataIndex: 'branch', key: 'branch' },
+    { title: 'Course', dataIndex: 'course', key: 'course' },
+    { title: 'Start Date', dataIndex: 'startDate', key: 'startDate' },
+    { title: 'End Date', dataIndex: 'endDate', key: 'endDate' },
+    {
+        title: 'Funding Source',
+        dataIndex: 'fundingSource',
+        key: 'fundingSource',
+    },
+    { title: 'Email', dataIndex: 'email', key: 'email' },
+    {
+        title: 'Email Personal',
+        dataIndex: 'emailPersonal',
+        key: 'emailPersonal',
+    },
+    { title: 'Facebook', dataIndex: 'facebook', key: 'facebook' },
+    { title: 'Instagram', dataIndex: 'instagram', key: 'instagram' },
+    { title: 'LinkedIn', dataIndex: 'linkedin', key: 'linkedin' },
 ];
 
 function OwnProfileView() {
-    return <Typography.Title level={2} style={{ textAlign: 'center' }}>
-        Profile
-    </Typography.Title>;
+    const auth = useAuth();
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(counter + 1);
+        }, 100);
+    }, []);
+
+    useEffect(() => {
+        console.log(counter);
+    }, [counter]);
+
+    return (
+        <Card
+            title={
+                <span>
+                    <UserOutlined /> My Profile
+                </span>
+            }
+        >
+            <Typography.Title level={5}>
+                Full name:{' '}
+                <Typography.Text>{`${auth.user.fullName}`}</Typography.Text>
+            </Typography.Title>
+
+            <br />
+            <Typography.Text>{`Email: ${auth.user.email}`}</Typography.Text>
+        </Card>
+    );
+
     // Put profile here
     // <Table dataSource={dataSource} columns={columns} />
 }
